b5f57647bfcd4ef961afecc6dc33d302c1a10c74
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-12-07 20:01:40 +0530

    Update setup_kerberos.sh

--- setup_kerberos.sh
+++ setup_kerberos.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo -e "[libdefaults]\ndefault_realm = EXAMPLE.COM" > /etc/krb5.conf
+# Predefine debconf responses to suppress interactive prompts
+export DEBIAN_FRONTEND=noninteractive
+
+# Install dependencies without prompts
+apt-get update && apt-get install -y krb5-user
+
+echo -e "[libdefaults]\ndefault_realm = vedji.COM" > /etc/krb5.conf
